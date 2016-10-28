ef586a17a9c85adf084a48a59c864c412ecab8cc
Rain Wang <rainwzp@gmail.com>
2016-10-28 18:22:01 +0800

    添加@Configuration测试

---
+++ macula-cart-repository/src/main/java/org/macula/cart/config/AppConfig.java
@@ -0,0 +1,22 @@
+/**
+ * AppConfiguration.java 2016年10月27日
+ */
+package org.macula.cart.config;
+
+import org.macula.core.config.MaculaAppConfig;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>
+ * <b>AppConfig</b> APP配置，用来代替macula-*-app.xml，也可以共存
+ * </p>
+ *
+ * @since 2016年10月27日
+ * @author Rain
+ * @version $Id$
+ */
+
+@Configuration
+public class AppConfig implements MaculaAppConfig {
+
+}
---
+++ macula-cart-repository/src/main/java/org/macula/cart/config/ServletConfig.java
@@ -0,0 +1,22 @@
+/**
+ * ServletConfiguration.java 2016年10月27日
+ */
+package org.macula.cart.config;
+
+import org.macula.core.config.MaculaServletConfig;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>
+ * <b>ServletConfig</b> Servlet配置，用来代替macula-*-servlet.xml，也可以共存
+ * </p>
+ *
+ * @since 2016年10月27日
+ * @author Rain
+ * @version $Id$
+ */
+
+@Configuration
+public class ServletConfig implements MaculaServletConfig {
+
+}
