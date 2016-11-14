3c82a03ef8ea16aefb6ff6e5d6fb55a6e1fed238
Rain Wang <rainwzp@gmail.com>
2016-11-14 09:07:25 +0800

    commit

--- macula-cart-api-impl/src/test/java/org/macula/cart/dubbo/DubboMain.java
+++ macula-cart-api-impl/src/test/java/org/macula/cart/dubbo/DubboMain.java
@@ -5,6 +5,8 @@ package org.macula.cart.dubbo;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -15,7 +17,7 @@ public class DubboMain {
 
 	private static final Log log = LogFactory.getLog(DubboMain.class);
 
-	public static void main(String[] args) {
+	public static void mainx(String[] args) {
 		try {
 			long startTime = System.currentTimeMillis();
 
@@ -41,6 +43,20 @@ public class DubboMain {
 					log.error("== synchronized error:", e);
 				}
 			}
+		}
+	}
+
+	public static void main(String args[]) {
+		String str = "estore-dev.infinitus.com.cn";
+		//String pattern = "^.+?(\\.[A-Za-z0-9\\.\\-]+)$";
+		String pattern = "^.+?(\\..*?[\\w\\-]+\\.[a-zA-Z]+)$";
+		
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(str);
+		if (m.matches()) {
+			System.out.println(m.group(0) + ",  " + m.group(1));
+		} else {
+			System.out.println("no match");
 		}
 	}
 
